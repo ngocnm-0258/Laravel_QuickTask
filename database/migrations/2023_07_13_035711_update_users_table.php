ebb5d62938296858da15a38d424b7941a1f65fc5
ngocnm-0258 <108354345+ngocnm-0258@users.noreply.github.com>
2023-07-13 15:04:57 +0700

    Update 2023_07_13_035711_update_users_table.php

--- database/migrations/2023_07_13_035711_update_users_table.php
+++ database/migrations/2023_07_13_035711_update_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
             $table->boolean('is_admin');
             $table->boolean('is_active');
             $table->string('username');
@@ -29,7 +28,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
             $table->dropColumn('is_admin');
             $table->dropColumn('is_active');
             $table->dropColumn('username');
